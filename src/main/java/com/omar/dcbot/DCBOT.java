f597b37abe96d3e0b67c1cb9534bbc0691ea6893
omaratakishi <omer.atakishiyev849@gmail.com>
2025-08-15 22:37:29 +0400

    init commit

---
+++ src/main/java/com/omar/dcbot/DCBOT.java
@@ -0,0 +1,33 @@
+package com.omar.dcbot;
+
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
+
+import javax.security.auth.login.LoginException;
+
+public class DCBOT {
+    private final ShardManager shardManager;
+
+    public DCBOT() throws LoginException {
+        String token = "MTQwNDA3NjIzMDczMDU4NDA2NA.G4jCKX.NaD7I2KnfClhbzpj9JNK38uudAf5FeJRl5AfAQ"; //Discord bot token
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
+        builder.setStatus(OnlineStatus.ONLINE);
+        builder.setActivity(Activity.playing("WITH YOUR EMOTIONS"));
+        shardManager = builder.build();
+
+    }
+
+    public ShardManager getShardManager(){
+        return shardManager;
+    }
+
+    public static void main(String[] args){
+        try{
+            DCBOT bot = new DCBOT();
+        } catch (LoginException e){
+            System.out.println("ERROR PROVIDED TOKEN IS INVALID!");
+        }
+    }
+}
